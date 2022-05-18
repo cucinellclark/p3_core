6fd664ef170b21d1bbb4000775d91088aade2f27
Clark Cucinell <clarkc@holly.mcs.anl.gov>
2022-05-18 14:12:11 -0500

    fixing bug for genome_id conversion

--- lib/bvbrc_api.py
+++ lib/bvbrc_api.py
@@ -31,7 +31,7 @@ def genome_id_float_to_str(row, genome_ids):
     del ret_row['Genome ID']
     for genome_id in genome_ids: 
         if genome_id in str(row['Genome ID']):
-            if 'Genome ID' in ret_row.columns: # if a genome_id has matched, check their lengths. The longer one is more specific
+            if 'Genome ID' in ret_row.keys(): # if a genome_id has matched, check their lengths. The longer one is more specific
                 if len(ret_row['Genome ID']) < len(genome_id): 
                     ret_row['Genome ID'] = genome_id
             else:
