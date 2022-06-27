d219a74e1cf3e0f1ccab0a5874c19cf0761d813a
Clark Cucinell <clarkc@holly.mcs.anl.gov>
2022-06-27 10:29:52 -0500

    testing

--- lib/bvbrc_api.py
+++ lib/bvbrc_api.py
@@ -114,10 +114,12 @@ def getPathwayDf(genome_ids,session,limit=2500000):
                 batch+=line
                 batch_count+=1 
         # TODO: set column data types
-        pathway_df = pd.read_csv(io.StringIO(batch),sep='\t',dtype={'genome_id':str})
+        pathway_df = pd.read_csv(io.StringIO(batch),sep='\t',dtype={'genome_id':str,'pathway_id':str})
         pathway_df_list.append(pathway_df)
     if len(pathway_df_list) > 0:
         return_df = pd.concat(pathway_df_list)
+        import pdb
+        pdb.set_trace()
         return return_df
     else:
         return None
